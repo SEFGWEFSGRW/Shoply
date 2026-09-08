13cd7eda5aa8f6ec154e276511677bc7edaa388e
SEFGWEFSGRW <midomax200479@gmail.com>
2026-09-08 12:43:58 +0000

    Fix routes import

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import router from "./routes";
+import router from "./routes/index.js";
 
 const app: Express = express();
 
